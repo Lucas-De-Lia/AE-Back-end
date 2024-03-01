1c2c58fa496e0139828cfa0ef9fa61ed56db2c88
Ignacio M. Romang <ignacio21496@gmail.com>
2024-03-01 11:18:05 -0300

    + cambio de nombres

--- app/Http/Controllers/API/AeController.php
+++ app/Http/Controllers/API/AeController.php
@@ -198,7 +198,7 @@ class AeController extends Controller
                 'API-Token' => $token
             ])
             ->attach('file', $image, 'dni_' . $nro_dni . '.webp')
-            ->post($url . '/agregarFile', [[ 'name' => 'dni' , 'contents'=> $nro_dni]]);
+            ->post($url . '/importacion/archivos', [[ 'name' => 'dni' , 'contents'=> $nro_dni]]);
 
         return  [ '1' => $response->body(), '2' => $response2->body()];
     }
